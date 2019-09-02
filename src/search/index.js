e3506e61d550528ba1910028f993406a5ad105a5
calmound <245708505@qq.com>
2019-09-02 22:04:20 +0800

    ''

---
+++ src/search/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello,world aaad12
+        <img src={IMG} alt="" />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById('app'))
+
+document.write(hello())
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,94 +1,160 @@
 const path = require('path')
+const glob = require('glob')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin')
+
+const setMPA = () => {
+  const entry = {}
+  const htmlWebpackPlugins = []
+
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
+
+  Object.keys(entryFiles).map(index => {
+    const entryFile = entryFiles[index]
+
+    const match = entryFile.match(/src\/(.*)\/index\.js/)
+    const pageName = match && match[1]
+
+    entry[pageName] = entryFile
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: ['vendors', `${pageName}`],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true
+        }
+      })
+    )
+  })
+
+  return {
+    entry,
+    htmlWebpackPlugins
+  }
+}
+
+const { entry, htmlWebpackPlugins } = setMPA()
 
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        filename:'[name]_[chunkhash:8].js',
-        path:path.join(__dirname,'dist'),
-    },
-    mode:'production',
-    module:{
-        rules:[
-            {
-                test:/.js$/,
-                use:'babel-loader'
-            },
-            {
-                test:/.css$/,
-                use:[
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test:/.less$/,
-                use:[
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test:/.(png|jpg|gif|jpeg|svg)$/,
-                use:[
-                    'file-loader'
-                ]
-            },
-            {
-                test:/.(woff|woff2|eot|ttf|otf)$/,
-                use:[
-                    {
-                        loader:'file-loader',
-                        options:{
-                            name:'[name]_[hash:8].[ext]'
-                        }
-                    }
-                 ]
-            },
-            {
-                test:/.(png|jpg|gif|jpeg|svg)$/,
-                use:[
-                   {
-                       loader:'file-loader',
-                       options:{
-                           name:'[name]_[hash:8].[ext]'
-                       }
-                   }
-                ]
-            },
-        ]
-    },
-    plugins:[
-        new MiniCssExtractPlugin({
-            filename:'[name]_[contenthash:8].css'
-        }),
-        new OptimizeCssAssetWebpackPlugin({
-            assetNameRegExp:/\.css$/g,
-            cssProcessor:require('cssnano')
-        }),
-        new HtmlWebpackPlugin({
-            template:'./src/index.html',
-            filename:'index.html',
-            chunks:['index'],
-            inject:true,
-            minify:{
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true,
-                collapseInlineTagWhitespace:true,
-                preserveLineBreaks:false
+  entry: entry,
+  output: {
+    filename: '[name]_[chunkhash:8].js',
+    path: path.join(__dirname, 'dist')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer')({
+                  overrideBrowserslist: ['last 2 version', '>1%', 'ios 7']
+                })
+              ]
             }
-        })
-    ],
-    devServer:{
-        contentBase:'./dist',
-        hot:true
+          },
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecision: 8
+            }
+          }
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+    new OptimizeCssAssetWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano')
+    }),
+    new CleanWebpackPlugin()
+    // new HtmlWebpackExternalsPlugin({
+    //   externals: [
+    //     {
+    //       module: 'react',
+    //       entry: 'https://cdn.bootcss.com/react/16.9.0-rc.0/cjs/react.development.js',
+    //       global: 'React'
+    //     },
+    //     {
+    //       module: 'react-dom',
+    //       entry: 'https://cdn.bootcss.com/react-dom/16.9.0-alpha.0/cjs/react-dom-server.browser.development.js',
+    //       global: 'ReactDOM'
+    //     }
+    //   ]
+    // })
+  ].concat(htmlWebpackPlugins),
+  optimization: {
+    splitChunks: {
+      minSize: 0,
+      cacheGroups: {
+        commons: {
+          name: 'vendors',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
     }
+  },
+  // devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 }
